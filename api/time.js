0a8d5f3746ff0028df575b524d20e310c0d0d545
The-Golden-Cheetah <159181250+The-Golden-Cheetah@users.noreply.github.com>
2026-02-20 21:19:18 -0600

    Update time.js

--- api/time.js
+++ api/time.js
@@ -11,5 +11,7 @@ export default function handler(req, res) {
     hour12: true
   });
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+  res.setHeader("Pragma", "no-cache");
   res.status(200).send(now);
 }
